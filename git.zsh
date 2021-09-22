958bdbaad4b4ba27e5ccf68defa85d308de7c614
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2021-09-22 17:01:09 +1000

    Add grc

--- git.zsh
+++ git.zsh
@@ -12,6 +12,16 @@ function gre() {
   git reset HEAD~$1
 }
 
+# Reset latest commit, then commit using its message
+# As a result, current non committed changes will be added to the last commit
+# Warning: changes git history
+function grc() {
+  local last_commit_message=$(git log -1 --pretty=%B)
+
+  gre
+  gcm "$last_commit_message"
+}
+
 function grho() {
   local branch=$(git branch --show-current)
   git reset --hard origin/$branch
