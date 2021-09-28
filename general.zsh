9e8f97dd4be1423b0dccb3c17ec5776f3641fa1b
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2021-09-28 17:52:58 +1000

    Refactor wsp

--- general.zsh
+++ general.zsh
@@ -1,6 +1,6 @@
 source "$ZSH/custom/utils.sh"
 
-alias dus="du -sh * | sort -h"
+alias dus="du -hs .* * --total 2> >(grep -v '^du: cannot access' >&2) | sort -h"
 alias gacd="gatsby clean && gatsby develop"
 alias gad="gatsby develop"
 alias inst="apt list --installed | grep"
@@ -23,30 +23,4 @@ function lsg() {
   fi
 
   ls -a "$location" | grep -i "$search"
-}
-
-function wsp() {
-  local JS_PROJECT_PATH="$(echo ~)/projects/javascript"
-  local TAMANU_PATH="$(echo ~)/bes/Tamanu/tamanu"
-  local TUPAIA_PATH="$(echo ~)/bes/Tupaia/tupaia"
-
-  if [ "$#" -lt 1 ]; then
-    log_error "Please provide a workspace!"
-    return 1
-  fi
-
-  local workspace
-  if [[ $(pwd) =~ $JS_PROJECT_PATH ]]; then
-    local project=$(command pwd | sed "s|$JS_PROJECT_PATH/||" | sed 's|/.*||')
-    workspace="@${project}/$*"
-  elif [[ $(pwd) =~ $TUPAIA_PATH ]]; then
-    workspace="@tupaia/$*"
-  elif [[ $(pwd) =~ $TAMANU_PATH ]]; then
-    workspace="$*"
-  else
-    workspace="@tupaia/$*"
-    run "cd $TUPAIA_PATH"
-  fi
-
-  run "yarn workspace $workspace"
 }
--- node.zsh
+++ node.zsh
@@ -32,19 +32,25 @@ function _np_clean() {
 }
 
 function _np_find() {
-  local USAGE="Usage: np find [options] <name>
+  local USAGE="Usage: np find [options...] <name>
 
 Options:
-  -r             Use regex
-  -h, --help     Show help
+  -p, --path        Path to search in (default: .)
+  -r, --regex       Use regex
+  -h, --help        Show help
 "
 
+  local path="."
   local search_op="-name"
   local name=""
 
   while [ "$1" != "" ]; do
     case $1 in
-    -r)
+    -p | --path)
+      shift
+      path="$1"
+      ;;
+    -r | --regex)
       shift
       search_op="-regex"
       ;;
@@ -64,5 +70,57 @@ Options:
     return 1
   fi
 
-  find . -path "*/node_modules" -prune , -path "*/dist" -prune , -type f $search_op "$name" | sort
+  find "$path" -path "*/node_modules" -prune , -path "*/dist" -prune , -type f $search_op "$name" | sort
+}
+
+function wsp() {
+  local USAGE="Usage: wsp <workspace> [options...] [command] [args...]
+
+Unless if specified otherwise, the script will run the specified command
+with its args from the current directory.
+
+If no options and args are provided, then the script will cd in <workspace>.
+
+Options:
+  -s                Will cd in <workspace> and run \"start\". Ignores command and args
+  -sd               Will cd in <workspace> and run \"start-dev\". Ignores command and args
+  -h, --help        Show help
+
+Usage examples:
+  wsp meditrak-server
+  wsp meditrak-server -s
+  wsp meditrak-server test --coverage
+"
+
+  local workspace="$1"
+
+  if [[ "$workspace" == "" ]]; then
+    echo $USAGE
+    return 1
+  fi
+
+  local project_path=""
+  if [[ $(pwd) =~ $JS_PROJECT_ROOT ]]; then
+    local project=$(command pwd | sed "s|$JS_PROJECT_ROOT/||" | sed 's|/.*||')
+    project_path="$JS_PROJECT_ROOT/$project"
+  elif [[ $(pwd) =~ $TUPAIA_ROOT ]]; then
+    local project=$(command pwd | sed "s|$TUPAIA_ROOT/||" | sed 's|/.*||')
+    project_path="$TUPAIA_ROOT/$project"
+  elif [[ $(pwd) =~ $TAMANU_ROOT ]]; then
+    local project=$(command pwd | sed "s|$TAMANU_ROOT/||" | sed 's|/.*||')
+    project_path="$TAMANU_ROOT/$project"
+  else
+    project_path="$TUPAIA_ROOT/tupaia/$project"
+  fi
+
+  if [[ $2 == "" ]]; then
+    run "cd $project_path/packages/$workspace"
+  elif [[ $2 == "-s" ]]; then
+    run "(cd $project_path/packages/$workspace && yarn start)"
+  elif [[ $2 == "-sd" ]]; then
+    run "(cd $project_path/packages/$workspace && yarn start-dev)"
+  else
+    shift
+    run "(cd $project_path/packages/$workspace && yarn $*)"
+  fi
 }
