d085eddde4489634807ca9e55c52d46ca7104491
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2022-02-02 06:02:22 +1100

    Add Atlassian

---
+++ atlassian.zsh
@@ -0,0 +1,1 @@
+alias at="cd $ATLASSIAN_ROOT"
--- config.zsh
+++ config.zsh
@@ -1,3 +1,4 @@
+ATLASSIAN_ROOT="$HOME/projects/atlassian"
 JS_PROJECT_ROOT="$HOME/projects/javascript"
 MEDITRAK_SSH_KEY="$HOME/.ssh/keys/meditrak-server.pem"
 TUPAIA_ROOT="$HOME/bes/Tupaia"
