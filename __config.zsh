c407f1a1762f650b6760e4d17d16b68dd7c6391e
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2022-02-02 06:09:42 +1100

    Load config before other scripts

---
+++ __config.zsh
@@ -0,0 +1,8 @@
+# Note: this file should be loaded first since other scripts depend on it.
+# Until a better solution is found, use "__" as a file name prefix since
+# oh-my-zsh seems to load files in `$ZSH_CUSTOM` alphabetically
+
+ATLASSIAN_ROOT="$HOME/projects/atlassian"
+JS_PROJECT_ROOT="$HOME/projects/javascript"
+MEDITRAK_SSH_KEY="$HOME/.ssh/keys/meditrak-server.pem"
+TUPAIA_ROOT="$HOME/bes/Tupaia"
